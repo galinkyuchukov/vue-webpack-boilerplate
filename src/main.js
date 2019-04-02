099aa83d95b6c47534e59ab6964ebf723f93bf3e
Galin <linderman83@gmail.com>
2019-04-02 21:47:16 +0300

    clear test es6 code that had to be transpiled by babel

--- src/main.js
+++ src/main.js
@@ -3,12 +3,6 @@ import App from './App.vue';
 
 import './assets/css/style.css'
 
-console.log('SEARCHME');
-var array = [1,2,3];
-Array.from(array).forEach(($item) => {
- console.log($item);
-})
-
 new Vue({
   el: '#app',
   render: h => h(App)
