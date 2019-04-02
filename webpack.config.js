13e703d3be9f4de86c35a5983dbb495855a244f4
Galin <linderman83@gmail.com>
2019-04-02 21:46:25 +0300

    added babel polyfill and babel-loader stuff

--- webpack.config.js
+++ webpack.config.js
@@ -40,12 +40,15 @@ module.exports = function(env){
                     ]
                 },
                 {
-                    test: /\.m?js$/,
+                    test: /\.js$/,
                     exclude: /(node_modules|bower_components)/,
                     use: {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['@babel/preset-env'] 
+                            presets: [['@babel/preset-env', {
+                                "useBuiltIns": "usage",
+                                "debug": true,                            
+                            }]]
                         }
                     }
                 }
